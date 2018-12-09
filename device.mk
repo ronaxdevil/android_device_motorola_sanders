d0dd74e4bc9de5b3a366ad300c1513c2bc1402db
therootlord <igor_cestari@hotmail.com>
2018-12-09 20:57:39 -0200

    sanders: update perf configs from LA.UM.7.6.2.r1-04200-89xx.0
    * init.qcom.rc: set vendor.post_boot.parsed prop, required for perf
    
    * perfboostsconfig.xml edits:
    
    * Reduced minimum launch boost freq
    * Remove SCHED_SPILL_NR_RUN and SCHED_RESTRICT_CLUSTER_SPILL opcodes
    
    Signed-off-by: ronaxdevil <pratabidya.007@gmail.com>

--- device.mk
+++ device.mk
@@ -309,6 +309,13 @@ DEVICE_PACKAGE_OVERLAYS += \
 PRODUCT_ENFORCE_RRO_TARGETS := \
     framework-res
 
+# Perf configs
+PRODUCT_COPY_FILES += \
+     $(LOCAL_PATH)/configs/perf/commonresourceconfigs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/perf/commonresourceconfigs.xml \
+     $(LOCAL_PATH)/configs/perf/perfboostsconfig.xml:$(TARGET_COPY_OUT_VENDOR)/etc/perf/perfboostsconfig.xml \
+     $(LOCAL_PATH)/configs/perf/targetconfig.xml:$(TARGET_COPY_OUT_VENDOR)/etc/perf/targetconfig.xml \
+     $(LOCAL_PATH)/configs/perf/targetresourceconfigs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/perf/targetresourceconfigs.xml
+
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power@1.0-impl \
@@ -317,8 +324,7 @@ PRODUCT_PACKAGES += \
 
 # Powerhint configuration file
 PRODUCT_COPY_FILES += \
-     $(LOCAL_PATH)/configs/powerhint.xml:system/etc/powerhint.xml \
-     $(LOCAL_PATH)/configs/perfboostsconfig.xml:$(TARGET_COPY_OUT_VENDOR)/etc/perf/perfboostsconfig.xml
+     $(LOCAL_PATH)/configs/powerhint.xml:system/etc/powerhint.xml
 
 # Qualcomm
 PRODUCT_COPY_FILES += \

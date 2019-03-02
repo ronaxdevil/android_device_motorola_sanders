b3708fa110e24d96e7e1b6adfa608f9668278e7c
ronaxdevil <pratabidya.007@gmail.com>
2019-03-02 15:13:37 +0000

    new havoc bring up

--- device.mk
+++ device.mk
@@ -470,5 +470,6 @@ PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
 
 PRODUCT_GMS_CLIENTID_BASE := android-motorola
 
-PRODUCT_VENDOR_KERNEL_HEADERS := hardware/qcom/msm8996/kernel-headers
+export export HAVOC_BUILD_TYPE=Official
+
 

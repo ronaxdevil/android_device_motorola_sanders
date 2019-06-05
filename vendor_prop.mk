67b5980fd77ec19fc354c744cd7cee2b46671223
ronaxdevil <pratabidya.007@gmail.com>
2019-06-05 20:18:03 +0000

    official tag
    Signed-off-by: ronaxdevil <pratabidya.007@gmail.com>

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -399,3 +399,11 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.config.dha_th_rate=2.3 \
     ro.config.sdha_apps_bg_max=64 \
     ro.config.sdha_apps_bg_min=8
+
+#IDKY
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.product.model=sanders
+
+# Maintainer
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.havoc.maintainer=RONAX
